a60f502d5ff73e4cc56ef5ee162ecdf00de742e5
brahimsaadi <brahim.saadi@g.enp.edu.dz>
2023-11-22 00:11:58 +0100

    solidity to do app functionalities

---
+++ README.md
@@ -0,0 +1,57 @@
+# TodoList Smart Contract Report
+
+## Overview
+This report provides an overview and analysis of the TodoList smart contract, a decentralized application (DApp) developed on the Ethereum blockchain using Solidity. The contract facilitates the management of tasks in a to-do list, offering functionalities such as adding, updating, deleting tasks, and retrieving task details.
+
+## Smart Contract Structure
+### 1. Task Structure
+The Task structure is well-defined with three key attributes:<br>
+
+`id`: A unique identifier for each task.<br>
+`content`: A string describing the task.<br>
+`completed`: A boolean indicating the task's completion status.<br>
+
+### 2. State Variables
+The contract efficiently utilizes state variables:<br>
+
+
+`owner`: The address of the contract owner.<br>
+`tasks`: A mapping to store tasks, where each task is identified by a unique ID.<br>
+`taskCount`: A counter for tracking the total number of tasks.<br>
+
+## Functions
+### 1. Modifier
+**onlyOwner Modifier**<br>
+Ensures that certain functions can only be called by the contract owner.
+
+### 2. Task Functions
+
+**addTask**<br>
+Allows the contract owner to add tasks to the to-do list.<br>
+
+**getTask**<br>
+Retrieves task details based on the provided task ID.<br>
+
+**getTaskByContent**<br>
+Retrieves task details based on the task content.<br>
+
+**updateTask**<br>
+Allows the owner to update task details, including content and completion status.<br>
+
+**markTaskCompleted**<br>
+Allows the owner to mark a task as completed.<br>
+
+**deleteTask**<br>
+Deletes a task by shifting tasks and reducing taskCount, with an update of subsequent task IDs.<br>
+
+**updateTaskId**<br>
+Updates the ID of a task, facilitating the smooth deletion and shifting of tasks.<br>
+
+**getAllTasks**<br>
+Retrieves details of all tasks in the to-do list.<br>
+
+
+### Resources
+
+[1] https://www.dappuniversity.com/articles/blockchain-app-tutorial#listTasks
+[2] https://medium.com/unitechie/build-todo-application-in-react-and-solidity-538ef2f1f54f
---
+++ to_do_app.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+contract TodoList {
+    address public owner;
+    
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+    uint256 public taskCount;
+
+    event TaskCreated(uint256 id, string content, bool completed);
+    event TaskUpdated(uint256 id, string content, bool completed);
+    event TaskCompleted(uint256 id);
+    event TaskDeleted(uint256 id);
+
+    constructor() {
+        owner = msg.sender;
+        taskCount = 0;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    function addTask(string memory _content) public onlyOwner {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+    function getTask(uint256 _id) public view returns (uint256, string memory, bool) {
+        require(_id > 0 && _id <= taskCount, "Invalid task ID");
+        Task memory task = tasks[_id];
+        return (task.id, task.content, task.completed);
+    }
+
+    function getTaskByContent(string memory _content) public view returns (uint256, string memory, bool) {
+        for (uint256 i = 1; i <= taskCount; i++) {
+            // Convert storage string to memory string for comparison
+            string memory taskContent = tasks[i].content;
+            if (keccak256(abi.encodePacked(taskContent)) == keccak256(abi.encodePacked(_content))) {
+                return (tasks[i].id, taskContent, tasks[i].completed);
+            }
+        }
+        revert("Task not found");
+    }
+
+    function markTaskCompleted(uint256 _id) public onlyOwner {
+        require(_id > 0 && _id <= taskCount, "Invalid task ID");
+        Task storage task = tasks[_id];
+        require(!task.completed, "Task is already completed");
+
+        task.completed = true;
+        emit TaskCompleted(_id);
+    }
+
+
+    function updateTask(uint256 _id, string memory _newContent, bool _newCompleted) public onlyOwner {
+        require(_id > 0 && _id <= taskCount, "Invalid task ID");
+        Task storage task = tasks[_id];
+        task.content = _newContent;
+        task.completed = _newCompleted;
+        emit TaskUpdated(_id, _newContent, _newCompleted);
+    }
+
+    function deleteTask(uint256 _id) public onlyOwner {
+        require(_id > 0 && _id <= taskCount, "Invalid task ID");
+
+        emit TaskDeleted(_id);
+
+        // Delete the task by shifting tasks and reducing taskCount
+        for (uint256 i = _id; i <= taskCount; i++) {
+            tasks[i] = tasks[i + 1];
+            updateTaskId(i, i - 1);
+        }
+
+        delete tasks[taskCount];
+        taskCount--;
+    }
+
+    function updateTaskId(uint256 _oldId, uint256 _newId) private onlyOwner {
+        require(_oldId > 0 && _oldId <= taskCount, "Invalid old task ID");
+        require(_newId > 0 && _newId <= taskCount, "Invalid new task ID");
+        require(_oldId != _newId, "New task ID must be different from the old ID");
+
+        Task storage task = tasks[_oldId];
+
+        // Create a new task with the new ID and copy content and completion status
+        tasks[_newId] = Task(_newId, task.content, task.completed);
+
+        // Emit TaskUpdated event for the new task
+        emit TaskUpdated(_newId, task.content, task.completed);
+    }
+
+    function getAllTasks() public view returns (uint256[] memory, string[] memory, bool[] memory) {
+        uint256[] memory taskIds = new uint256[](taskCount);
+        string[] memory taskContents = new string[](taskCount);
+        bool[] memory taskCompletedStatus = new bool[](taskCount);
+
+        for (uint256 i = 1; i <= taskCount; i++) {
+            taskIds[i - 1] = tasks[i].id;
+            taskContents[i - 1] = tasks[i].content;
+            taskCompletedStatus[i - 1] = tasks[i].completed;
+        }
+
+        return (taskIds, taskContents, taskCompletedStatus);
+    }
+}
